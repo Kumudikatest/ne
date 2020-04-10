481bb7f85672e2215d985e840d5eb3d60c097d19
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-10 13:21:20 +0530

    test

--- ne.js
+++ ne.js
@@ -1,26 +1,4 @@
-let google = require('googleapis').google;
-let _auth = require('./Authorizer');
-const datastore = google.datastore('v1');
-
 exports.handler = function (request, response) {
-    datastore.projects.beginTransaction({
-        projectId: process.env.GCP_PROJECT,
-        resource: {
-            transactionOptions: {
-                readWrite: {}
-            }
-        }
-    }).then(response => {
-        console.log(response.data);           // successful response
-        /*
-        response.data = {
-            "transaction": "<transaction ID>"
-        }
-        */
-    })
-        .catch(err => {
-            console.log(err, err.stack); // an error occurred
-        });
 
     response.send({ "message": "Successfully executed" });
 }
